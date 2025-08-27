b64759f78ef84b7e7d1b5fb8e61ed8bc98e51693
Eduardo Reis <eduardo.olvreis@gmail.com>
2025-08-27 17:22:46 -0300

    Adição do hero e estilizações gerais

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -9,19 +9,19 @@ export default function Header(){
     }
     
     return(
-        <header>
-            <a href="" id="logo">
-                <img src="../src/assets/images/logoReto.png" alt="Logo principal do portfolio" />
-            </a>
+        <header>   
+            <div className="headerLogo">
+                <img src="../src/assets/images/logo.svg" alt="Logo principal do portfolio" />
+            </div>
             <nav>
                 <ul className={visivel? "menu aberto" : "menu"}>
-                    <li><a href="">Sobre Mim</a></li>
-                    <li><a href="">Projetos</a></li>
-                    <li><a href="">Contato</a></li>
+                    <li><a href=""><p className="itemMenu">Sobre Mim</p></a></li>
+                    <li><a href=""><p className="itemMenu">Projetos</p></a></li>
+                    <li><a href=""><p className="itemMenu">Contato</p></a></li>
                 </ul>
                 <button className={visivel? "botaoFlip aberto" : "botaoFlip"} onClick={toggleMenu}>
-                    <div className="front"><img src="../src/assets/images/icons/icone-menu.svg"/></div>
-                    <div className="back"><img src="../src/assets/images/icons/icone-fechar.svg"/></div>
+                    <div className="front"><img src="../src/assets/images/icons/menu-icon.svg"/></div>
+                    <div className="back"><img src="../src/assets/images/icons/close-icon.svg"/></div>
                 </button>
             </nav>
         </header>
---
+++ portfolio/src/components/MainSection.jsx
@@ -0,0 +1,19 @@
+export default function MainSection(){
+    return(
+        <main>
+            <div className="hero">
+                <div className="heroText">
+                    <h1>
+                        <span>
+                            F R O N T E N D
+                            <br />
+                            D E V E L O P E R
+                        </span>
+                    </h1>
+                    <p>Desenvolvo soluções frontend modernas, unindo performance e usabilidade.</p>
+                </div>
+                <img src="../src/assets/images/heroImage.svg" alt="Imagem de um computador antigo com a minha logo" />
+            </div>
+        </main>
+    )
+}
