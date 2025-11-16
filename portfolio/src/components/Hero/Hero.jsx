d5861c4a359bc2592a4c2adb16eed3fd2e91f583
eduardo.reis <eduardo.olvreis@gmail.com>
2025-11-16 20:35:54 -0300

    style: (Hero) Realizada a responsividade 992px - 1199px e ajustes no Header

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -7,7 +7,12 @@ export default function Hero(){
                 <h2>
                     F R O N T E N D <span className={style.quebraLinha}>D E V E L O P E R</span>
                 </h2>
-                <p>Me chamo <span className="textImportant">Eduardo Reis</span> e desenvolvo soluções frontend modernas, unindo performance e usabilidade.</p>
+
+                {/* Texto para versões Mobile e Tablet */}
+                <p className={style.mobile}>Me chamo <span className="textImportant">Eduardo Reis</span> e desenvolvo soluções frontend modernas, unindo performance e usabilidade.</p>
+
+                {/* Texto para versões Desktop e Tablet paisagem*/}
+                <p className={style.desktop}>Me chamo <span className="textImportant">Eduardo Reis</span> desenvolvedor frontend focado em criar interfaces modernas, performáticas e centradas no usuário. Trabalho com <span className="textImportant">HTML, CSS, JavaScript, React e TypeScript </span>para construir aplicações rápidas, acessíveis e visualmente refinadas.</p>
             </div>
             <div className={style.containerImage}>
                 <img src="../src/assets/images/heroImage.svg" alt="Imagem de um computador antigo com a minha logo" />
