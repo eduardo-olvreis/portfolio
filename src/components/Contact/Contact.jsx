57ca683872203784fff6daf3f15401875cec8bc7
Eduardo <eduardo.olvreis@gmail.com>
2026-02-13 00:08:30 -0300

    feat: componente ContactForm start

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import style from "./Contact.module.css"
+import ContactForm from "./ContactForm"
 
 export default function Contact(){
     return(
         <section id="contact-section" className={style.container}>
             <div className={style.content}>
                 <h2>ENTRE EM <br />CONTATO</h2>
+                <ContactForm/>
                 <ul className={style.list}>
                         <li className="contactItem"><a href="https://www.linkedin.com/in/eduardo-olvreis/" target="__blank"><img src="../src/assets/images/icons/linkedin_Icon.svg" alt="Link para o meu Linkedin" /><p>Eduardo Reis</p></a></li>
                         <li className="contactItem"><a href="https://github.com/eduardo-olvreis" target="__blank"><img src="../src/assets/images/icons/github_icon.svg" alt="Link para o meu GitHub" /><p>eduardo-olvreis</p></a></li>
---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,14 @@
+export default function ContactForm(){
+  return(
+    <div>
+      <label htmlFor="">Nome</label>
+      <input type="text" placeholder="Seu Nome"/>
+
+      <label htmlFor="">Email</label>
+      <input type="text" placeholder="Seu E-mail" />
+
+      <label htmlFor="">Mensagem</label>
+      <input type="text" placeholder="Mensagem"/>
+    </div>
+  )
+}
