0ee3a476429f90ffe9054606b2e3623e7afff049
Eduardo <eduardo.olvreis@gmail.com>
2026-02-15 13:24:50 -0300

    style: responsividade completa do Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero(){
         <div className={style.containerHero}>
             <div className={style.text}>
                 <h2>
-                    F R O N T E N D <span className={style.quebraLinha}>D E V E L O P E R</span>
+                    F U L L  S T A C K<span className={style.quebraLinha}>D E V E L O P E R</span>
                 </h2>
                 <p className={style.mobile}>Me chamo <span className="textImportant">Eduardo Reis</span> e desenvolvo soluções frontend modernas, unindo performance e usabilidade.</p>
             </div>
